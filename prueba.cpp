7450ac4fe6e91c207adb6e38b1f479d296235355
vivikunin <vivikunin@gmail.com>
2025-03-17 18:24:50 -0300

    Creacion de clases

--- prueba.cpp
+++ prueba.cpp
@@ -1,1 +1,14 @@
-hola
+#include <string>
+#include "Publicacion.h"
+
+using namespace std;
+
+class PaginaWeb: public Publicacion{
+    private:
+        string url;
+        string contenidoExtraido;
+    public:
+        PaginaWeb(string, string, DTFecha,string, string);
+        ~PaginaWeb();
+        virtual bool contienePalabra(std::string);
+};
